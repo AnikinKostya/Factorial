971b70a96aafccf9f6cdeef3b0cb71a1dbb3c649
AnikinKostya <52211374+AnikinKostya@users.noreply.github.com>
2019-06-26 16:05:25 +0400

    Add files via upload

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Введите число n: ");
+
+            n = int.Parse(Console.ReadLine());
+
+            int factorial = 1;   
+
+            for (int i = 2; i <= n; i++) 
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine(factorial);
+            Console.ReadLine();
+        }
+    }
+}
